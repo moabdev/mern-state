c4c724f65103acdaf24dcc19255761fbc4eba79b
Moab Macena <macenamoab3@gmail.com>
2023-11-25 20:27:12 -0300

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignOut from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import { About } from "./pages/About"
+
+
+
+
 export default function App() {
   return (
-    <h1 className="ttext-red-500 -3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/rofile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
@@ -17,10 +33,12 @@ export default function App() {
   My server is going to run inside this URL: http://localhost:5173 I can change the host on package .json  
 
   git init
-
   git add.
-
   gitcommit -m "install react js and tailwind css and create the first template"
+  Now you can go to https://github.com/ and create a new repository.
+  git remote add origin git@github.com:moabdev/mern-state.git
+  git branch -M main
+  git push -u origin main
 
 
 
