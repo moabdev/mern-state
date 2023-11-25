8884646c083eb15f45b554200fc45235837bf175
Moab Macena <macenamoab3@gmail.com>
2023-11-25 20:02:22 -0300

    install react js and tailwind css and create the first template

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+export default function App() {
+  return (
+    <h1 className="ttext-red-500 -3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+
+/*
+
+  Tailwind.config.js
+  This is basically is going to check the index.html file, and also all the files with these extensions inside this folder, source folder. ANd any classes I add, any tailwind CSS class I add is going to be detected and then the CSS equivalent of that is going to be created.
+
+  console log ninja -> It's for console log inside of the visual studio
+
+  My server is going to run inside this URL: http://localhost:5173 I can change the host on package .json  
+
+  git init
+
+  git add.
+
+  gitcommit -m "install react js and tailwind css and create the first template"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
