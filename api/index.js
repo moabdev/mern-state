e3c642da81577d7b02a193f621358470b56cdf1d
Moab Macena <macenamoab3@gmail.com>
2023-11-26 14:18:52 -0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then( () => {
+        console.log('Connected to MongoDB!')
+    }).catch( (err) => {
+        console.log(err)
+    });
+
 
 const app = express();
 
@@ -6,7 +19,7 @@ app.listen(3000, () => {
     console.log('Server app is running on port 3000!')
 });
 
-
+//we're goint to install the package mongoose to easily interact to our database, create models.
 
 
 
