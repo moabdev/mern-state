61858e959664de4a50b963a4178015f60f510d34
Moab Macena <macenamoab3@gmail.com>
2023-11-26 14:39:21 -0300

    create a user model

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// Using mongoose we're going to create a schema/the rules for our user.
+//This is going to protext the data that we want to add to our database.
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6,
+    }
+}, {timestamps: true});
+
+//timestamps tells mongoose that we want to keep track of when the data was added to our database..
+// We can use this two extra informmation to be able to easily sort them by time and latest.
+
+// So after we have created our schema we can create a model for our database.
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
