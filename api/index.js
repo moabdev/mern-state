167818a0495e8ed17027f62cc0c5bd3a95a79ced
Moab Macena <macenamoab3@gmail.com>
2023-11-25 22:48:29 -0300

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server app is running on port 3000!')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
